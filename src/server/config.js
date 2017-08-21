c0424142074e59dd1eebd4f77327ee4bf7d6ecca
zjf <zjf123312@163.com>
2017-08-21 15:18:18 +0800

    api url配置

--- src/server/config.js
+++ src/server/config.js
@@ -6,12 +6,8 @@ if(window.location.host=='www.test.com'){
     _APIurl = 'http://api.test.com'
 }else if(window.location.host=='pc.test.com'){
     _APIurl = 'http://api.test.com'
-}else if(window.location.host=='testapi.zzflgs.cn'){
-    _APIurl = 'http://testapi.zzflgs.cn'
-}else if(window.location.host=='jkyr.yearnedu.com'){
-    _APIurl = 'http://jkyr.yearnedu.com'
 }else{
-    _APIurl = 'http://testapi.zzflgs.cn'
+    _APIurl = ''
 }
 
 export default _APIurl
