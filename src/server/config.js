7d06eed3aa65cf2e3bd5e7fb870e70f3d5ca648c
zjf <zjf123312@163.com>
2017-08-21 10:18:07 +0800

    config

--- src/server/config.js
+++ src/server/config.js
@@ -8,6 +8,8 @@ if(window.location.host=='www.test.com'){
     _APIurl = 'http://api.test.com'
 }else if(window.location.host=='testapi.zzflgs.cn'){
     _APIurl = 'http://testapi.zzflgs.cn'
+}else if(window.location.host=='jkyr.yearnedu.com'){
+    _APIurl = 'http://jkyr.yearnedu.com'
 }else{
     _APIurl = 'http://testapi.zzflgs.cn'
 }
