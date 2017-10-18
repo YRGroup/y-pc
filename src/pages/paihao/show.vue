5ce6194abdd789b80bb407f4b6941e3449bff332
GGwangxin <975769880@qq.com>
2017-10-18 17:40:56 +0800

    排号优化

--- src/pages/paihao/show.vue
+++ src/pages/paihao/show.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="main" style="position: relative;z-index: 9">
       <div class="title">
-        2017年第一次期中考试
+        {{nowObj.Name}}
       </div>
       <div class="now">
         正在考试
@@ -138,7 +138,7 @@
 
       },
       getInterval(){
-        setTimeout(this.QueueGet,500)
+        setTimeout(this.QueueGet,3000)
       },
       QueueGet(){
         this.getInterval();
